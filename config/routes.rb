f1646ac0b47b8fbbc483ee2e8976b3be80f771b9
pabloamco <pablol@amcoonline.net>
2020-12-29 04:44:56 -0600

    linked in omniauth first part

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   scope '/(:locale)', locale: /en|es/ do
-    root 'main#index'
-    resources :static_pages, only: [] do
+    root 'static_pages#document'
+    resources :users, only: [] do
       collection do
         get 'document'
       end
